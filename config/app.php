4bbce3f649a0c100c63fc201145c295b6f8bedfe
hrdaya <info@mar-cielo.com>
2016-07-30 12:17:12 +0900

    laravelcollective/htmlの導入

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
          * Composer Installed Settings
          */
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -206,6 +207,12 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+         * Composer Installed Settings
+         */
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
