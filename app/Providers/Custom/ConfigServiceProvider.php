ab60bb0b12a352d385161b056d55a29e3e1def0b
hrdaya <info@mar-cielo.com>
2016-07-30 18:21:52 +0900

    設定ファイルの変更 #27

---
+++ app/Providers/Custom/ConfigServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers\Custom;
+
+use Illuminate\Support\ServiceProvider;
+
+class ConfigServiceProvider extends ServiceProvider
+{
+
+    /**
+     * サービス初期処理登録後に実行
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $configs = config(\App::environment());
+        if (!is_null($configs)) {
+            foreach ($configs as $key => $value) {
+                $config = config($key) ?: [];
+                config([$key => array_merge($config, $value)]);
+            }
+        }
+    }
+
+    /**
+     * アプリケーションサービスの登録
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+}
