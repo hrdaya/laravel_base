c6b41cc05948970e9afa030c4a7224fd48b2f6f8
hrdaya <info@mar-cielo.com>
2016-07-30 12:32:20 +0900

    Excel、CSVファイルを扱うライブラリの導入

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Collective\Bus\BusServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
 
@@ -214,6 +215,7 @@ return [
          */
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
