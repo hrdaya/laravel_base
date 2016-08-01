957b6839ca0e4b089e92cd8aa54f7c801eaf030c
hrdaya <info@mar-cielo.com>
2016-08-01 23:38:51 +0900

    カスタムバリデータの追加 #58

---
+++ app/Libs/CustomValidator.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Libs;
+
+use Illuminate\Validation\Validator;
+
+/*
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 独自バリデータクラス
+ * ---------------------------------------------------------------------------------------------------------------------
+ *
+ * 各バリデータには「validate」プレフィクスを付けることが必要です
+ * 例) public function validateCheckDigit($attribute, $value, $parameters) {}
+ *
+ * 上記のバリデーションを使用する際には「checkDigit」を指定します
+ *
+ * バリデーションのメッセージは「resources/lang/ja/validation.php」内に記述してください
+ */
+
+class CustomValidator extends Validator
+{
+
+    /**
+     * JANコード・ITFコード等のチェックデジットを確認する
+     *
+     * @param string $attribute  フィールド名
+     * @param mixed  $value      検証する値
+     * @param array  $parameters オプションパラメータ
+     *
+     * @return bool
+     */
+    public function validateCheckDigit($attribute, $value, $parameters)
+    {
+        $arr = str_split($value);
+        // 偶数の和
+        $even = 0;
+        // 奇数の和
+        $odd = 0;
+        for ($i = 0, $len = count($arr) - 1; $i < $len; $i++) {
+            if (($i + 1) % 2 == 0) {
+                $even += (int)$arr[$i];
+            } else {
+                $odd += (int)$arr[$i];
+            }
+        }
+        // 偶数の和を3倍+奇数の総和を加算して、下1桁の数字を10から引く
+        $cd = 10 - (int)substr((string)(($even * 3) + $odd), -1);
+        // 10なら1の位は0なので、0にする
+        $cd = $cd === 10 ? '0' : (string)$cd;
+
+        return substr($value, -1) === $cd;
+    }
+
+}
---
+++ app/Providers/Custom/ValidatorServiceProvider.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Providers\Custom;
+
+use App\Libs\CustomValidator;
+use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class ValidatorServiceProvider extends ServiceProvider
+{
+
+    /**
+     * アプリケーションサービスの初期処理
+     *
+     * @return void
+     */
+    protected function boot()
+    {
+        /**
+         * Create a new Validator instance.
+         *
+         * @param  \Symfony\Component\Translation\TranslatorInterface $translator
+         * @param  array                                              $data
+         * @param  array                                              $rules
+         * @param  array                                              $messages
+         * @param  array                                              $customAttributes
+         *
+         * @return void
+         */
+        $this->app['validator']->resolver(
+            function (
+                TranslatorInterface $translator,
+                array $data,
+                array $rules,
+                array $messages = [],
+                array $customAttributes = []
+            ) {
+                return new CustomValidator($translator, $data, $rules, $messages, $customAttributes);
+            }
+        );
+    }
+
+    /**
+     * アプリケーションサービスの登録
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+}
