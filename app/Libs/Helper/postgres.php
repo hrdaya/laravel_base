62b2df275955e868dad381b7c3871a6fbd32df26
hrdaya <info@mar-cielo.com>
2016-08-01 23:24:54 +0900

    postgres用のヘルパー関数の作成 #50

---
+++ app/Libs/Helper/postgres.php
@@ -0,0 +1,21 @@
+<?php
+
+// PHPの配列からPostgresの配列への変換
+if (function_exists('toPgArray')) {
+
+    function toPgArray(string $value)
+    {
+        return '{'.implode(',', $value).'}';
+    }
+
+}
+
+// Postgresの配列をPHPの配列に変換
+if (function_exists('fromPgArray')) {
+
+    function fromPgArray(string $value)
+    {
+        return explode(',', trim($value, '{}'));
+    }
+
+}
