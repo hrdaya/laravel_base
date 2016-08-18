90ab60a992e2316af0a1aebc346a36dc0e3495ba
hrdaya <info@mar-cielo.com>
2016-08-18 21:41:07 +0900

    #92 のコミットするファイルを間違えていたのを修正

--- app/Http/Resources/js/ajax.js
+++ app/Http/Resources/js/ajax.js
@@ -5,28 +5,32 @@ $(function () {
   var showLoading = function () {
     $('#loader-bg').addClass('show');
   };
+
   // ローディングの非表示
   var hideLoading = function () {
     $('#loader-bg').removeClass('show');
   };
+
   // Ajaxの初期設定
   $.ajaxSetup({
-    type: 'POST',
+    type    : 'POST',
     dataType: 'json',
-    timeout: 30000,
-    headers: {
-      'pragma': 'no-cache',
-      'Cache-Control': 'no-cache',
+    timeout : 30000,
+    headers : {
+      pragma             : 'no-cache',
+      'Cache-Control'    : 'no-cache',
       'If-Modified-Since': 'Thu, 01 Jun 1970 00:00:00 GMT',
-      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      'X-CSRF-TOKEN'     : $('meta[name="csrf-token"]').attr('content')
     }
   });
+
   // ajaxの中断処理（別のページへの移動時）
   $('body').on('ajaxSend', function (c, xhr) {
     $(window).on('beforeunload', function () {
       xhr.abort();
     });
   });
+
   // グローバルのイベントハンドラ
   $(document).ajaxSend(function () {
     // event, jqXHR, ajaxOptions
@@ -36,9 +40,9 @@ $(function () {
     // event, jqXHR, ajaxOptions
     // ローディングの非表示
     hideLoading(); // 適当に関数を作っておく
-  }).ajaxError(function (event, jqXHR, settings, exception) {
-    if (jqXHR.status) {
-      switch (jqXHR.status) {
+  }).ajaxError(function (event, jqXhr, settings, exception) {
+    if (jqXhr.status) {
+      switch (jqXhr.status) {
         case 404:
           // ステータスコードが404の時
 
@@ -63,7 +67,7 @@ $(function () {
       return;
     } else {
       // その他のエラー
-      throw new Error(jqXHR.status + ' ' + exception);
+      throw new Error(jqXhr.status + ' ' + exception);
     }
   });
 });
